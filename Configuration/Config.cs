d67542f7a373d48ae63ca84669d1814e13660ae0
Swan <me@swan.moe>
2020-05-28 10:45:39 -0400

    Initial Commit

---
+++ Configuration/Config.cs
@@ -0,0 +1,67 @@
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Quaver.Steam.Deploy.Configuration
+{
+    public class Config
+    {
+        /// <summary>
+        ///     The directory of where Quaver is housed.
+        /// </summary>
+        public string QuaverProjectDirectory { get; set; } = "";
+
+        /// <summary>
+        ///     Whether or not the tool should zip up the builds
+        /// </summary>
+        public bool ZipBuilds { get; set; }
+
+        /// <summary>
+        ///     The path of the config file.
+        /// </summary>
+        public static string Path => $"{Directory.GetCurrentDirectory()}/config.json";
+
+        /// <summary>
+        ///     Deserializes the config into an object.
+        /// </summary>
+        /// <returns></returns>
+        public static Config Deserialize()
+        {
+            const string path = "./config.json";
+
+            // If the file doesn't exist, then we'll want to create the file, then throw a FileNotFoundException
+            if (!File.Exists(path))
+            {
+                var config = new Config();
+                config.Save();
+
+                throw new FileNotFoundException("config.json file was not found. A template has been created for you.");
+            }
+
+            Config parsedConfig;
+
+            // Deserialize it if it already exists.
+            using (var file = File.OpenText(path))
+            {
+                var serializer = new JsonSerializer();
+                parsedConfig = (Config)serializer.Deserialize(file, typeof(Config));
+            }
+
+            // Do an initial save on the config.
+            parsedConfig.Save();
+
+            return parsedConfig;
+        }
+        
+        /// <summary>
+        ///     Saves the configuration
+        /// </summary>
+        private void Save()
+        {
+            using (var sw = new StreamWriter(Path))
+            {
+                var output = JsonConvert.SerializeObject(this, Formatting.Indented);
+                sw.Write(output);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Quaver.Steam.Deploy.Configuration;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+
+namespace Quaver.Steam.Deploy
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The output directory of the build
+        /// </summary>
+        private static string OutputDir => Directory.GetCurrentDirectory() + "/build";
+
+        /// <summary>
+        ///     The version of the client that's being built
+        /// </summary>
+        private static string Version { get; set; }
+
+        /// <summary>
+        ///     Config setup for the deploy script to use
+        /// </summary>
+        private static Config Configuration { get; set; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="args"></param>
+        internal static void Main(string[] args)
+        {
+            Configuration = Config.Deserialize();
+            DeleteExistingBuild();
+            
+            Version = GetVersion();
+            CompileClients();
+        }
+
+        /// <summary>
+        ///     Deletes the output folder if it exists to start fresh.
+        /// </summary>
+        private static void DeleteExistingBuild()
+        {
+            if (Directory.Exists(OutputDir))
+                Directory.Delete(OutputDir, true);
+
+            Directory.CreateDirectory(OutputDir);
+        }
+        
+        /// <summary>
+        ///     Asks the user for a version number of the client
+        /// </summary>
+        /// <returns></returns>
+        private static string GetVersion()
+        {
+            Console.Write("Enter a version number for the client: ");
+
+            string version = null;
+
+            while (string.IsNullOrEmpty(version))
+                version = Console.ReadLine();
+
+            return version;
+        }
+
+        /// <summary>
+        ///     Compiles the client for each platform
+        /// </summary>
+        private static void CompileClients()
+        {
+            var platforms = new[]
+            {
+                "win-x64",
+                "linux-x64",
+                "osx-x64"
+            };
+
+            foreach (var platform in platforms)
+            {
+                var ver = $"'{Version}' for {platform}";
+
+                Console.WriteLine($"Compiling Quaver version {ver}...");
+
+                var dir = $"{OutputDir}/{platform}";
+                Directory.CreateDirectory(dir);
+
+                var cmd = $"publish {Configuration.QuaverProjectDirectory} -r {platform} -c Public -o {dir}";
+                RunCommand("dotnet", cmd, false);
+
+                Console.WriteLine($"Finished compiling Quaver version {ver}!");
+
+                if (!Configuration.ZipBuilds) 
+                    continue;
+                
+                Console.WriteLine($"Archiving Quaver version {ver} to a zip file...");
+
+                // For debug/offline builds on Steam. Needs a Spacewar file
+                File.WriteAllText($"{dir}/steam_appid.txt", "480");
+                
+                using (var archive = ZipArchive.Create())
+                {
+                    archive.AddAllFromDirectory(dir);
+                    archive.SaveTo($"{Directory.GetCurrentDirectory()}/quaver-{Version}-{platform}.zip", CompressionType.Deflate);
+                }
+                
+                Console.WriteLine($"Finished archiving Quaver version {ver} to a zip file!");
+            }
+            
+            Console.WriteLine("Successfully finished compiling all Quaver versions!");
+        }
+        
+        /// <summary>
+        ///     Runs a CLI command
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static bool RunCommand(string command, string args, bool showOutput = true)
+        {
+            var psi = new ProcessStartInfo(command, args)
+            {
+                WorkingDirectory = Environment.CurrentDirectory,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            var p = Process.Start(psi);
+            
+            if (p == null) 
+                return false;
+
+            var output = "";
+            
+            output += p.StandardOutput.ReadToEnd();
+            output += p.StandardError.ReadToEnd();
+
+            p.WaitForExit();
+
+            if (p.ExitCode == 0) 
+                return true;
+
+            Console.WriteLine(output);
+            return false;
+        }
+    }
+}
